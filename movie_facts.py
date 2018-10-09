9fb75fa41e7449b93b6927c26e4612f955bc1a51
StellaMarieL <42846565+StellaMarieL@users.noreply.github.com>
2018-10-09 22:01:31 +0200

    Update movie_facts.py

--- movie_facts.py
+++ movie_facts.py
@@ -1,3 +1,35 @@
 #simple version
 movie = {
- 
+    "title" : "The Dark Knight",
+    "year" : 2008,
+    "duration" : 152,
+    "director" : "Christopher Nolan"
+    }
+
+for key, value in movie.items():
+    print(f{'key'} : {'value'})
+
+#doing the same thing, but adding minutes to the duration
+movie = {
+    "title" : "The Dark Knight",
+    "year" : 2008,
+    "duration" : 152,
+    "director" : "Christopher Nolan"
+    }
+
+for key, value in movie.items():
+    if key == "duration":
+        print(f{'key'} : {'value'} minutes)
+    else:
+        print(f{'key'} : {'value'})
+
+#adding the actors
+movie["actors"] = ["Heath Ledger", "Christian Bale", "Aaron Eckhart", "Michael Caine", "Maggie Gyllenhaal", "Gary Oldman", "Morgan Freeman"]
+
+for key, value in movie.items():
+    if key == "duration":
+        print(f{'key'} : {'value'} minutes)
+    elif key == "actors"
+        print(f{'key'} + ":" + ", ".join(value))
+    else:
+        print(f{'key'} : {'value'})
