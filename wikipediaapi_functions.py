a5f60134cff8f06231880a0bd4289a0f539633cc
StellaMarieL <42846565+StellaMarieL@users.noreply.github.com>
2018-10-22 11:06:21 +0200

    Create wikipediaapi_functions.py

---
+++ wikipediaapi_functions.py
@@ -0,0 +1,18 @@
+import requests
+
+def lookup(title, value):
+    url = f"https://en.wikipedia.org/api/rest_v1/page/summary/{title}"
+    req = requests.get(url)
+    data = req.json()
+    user_input = data[f"{value}"]
+    return user_input
+
+#Ask the user for a wikipedia article and if they want a description or extract
+title = input("What would you like to look for on wikipedia?")
+title.strip().replace(" ", "_")
+
+value = input("Would you like to view a 'description' or a 'extract'? Please choose one of the two: ")
+value.lower().strip()
+
+data = lookup(title, value)
+print(data)
